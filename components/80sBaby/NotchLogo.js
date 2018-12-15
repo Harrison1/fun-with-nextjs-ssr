988dec1378a66ad60fb515b84ccc25615662ca2c
Harrison1 <hmcguire13@gmail.com>
2018-12-15 06:32:29 -0500

    added icons

--- components/80sBaby/NotchLogo.js
+++ components/80sBaby/NotchLogo.js
@@ -7,8 +7,10 @@ const NotchLogo = () =>
     <style jsx>{`
       div {
         background: #fff;
+        border: 4px solid #222;
         border-radius: 0 0 10px 10px;
-        box-shadow: 0 2px 2px 2px rgba(0, 0, 0, 0.3);
+        border-top: none;
+        // box-shadow: 0 2px 2px 2px rgba(0, 0, 0, 0.3);
         padding: 2px 50px; 
       }
     `}</style>
--- components/App.js
+++ components/App.js
@@ -5,6 +5,11 @@ import Drawer from './80sBaby/Drawer'
 import FeaturedImage from './80sBaby/FeatureImage'
 import FeatureRow from './80sBaby/FeatureRow'
 import ToggleDrawer from './ToggleDrawer'
+import Person from './assets/Person'
+import DarthVader from './assets/DarthVader'
+import Book from './assets/Book'
+import BobaFett from './assets/BobaFett'
+import DeathStar from './assets/DeathStar'
 
 const App = props =>
   <Layout>
@@ -12,6 +17,11 @@ const App = props =>
     <Blogroll posts={ props.posts } />
     <Drawer />
     <ToggleDrawer />
+    <Person />
+    <DarthVader />
+    <Book />
+    <BobaFett />
+    <DeathStar />
   </Layout>
 
 export default App
